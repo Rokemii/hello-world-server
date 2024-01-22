dceb2caeff9f291626eacb039262985c70fd25d6
Roque Rojo Bacete <rojobaceteroque@gmail.com>
2024-01-22 23:41:49 +0100

    Update for activity2

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const crypto = require("crypto");
 
 const id = crypto.randomBytes(16).toString("hex");
 app.get('/', (req, res) => {
-  var txt = fs.readFileSync("./private_ip_mongo.txt");
-  res.send(`Hola mundo!. Soy Roque Rojo Bacete y he desarrollado esto para el master DevOps de UNIR\nSoy la instancia: ${id}\nEl connection string de mongo es: mongodb://${txt}:27017/`);
+  var txt = fs.readFileSync("./private_ip_mongo.txt").replace(/(\r\n|\n|\r)/gm, '');
+  res.send(`Hola mundo!. Soy Roque Rojo Bacete y he desarrollado esto para el master DevOps de UNIR\nSoy la instancia: ${id}\nEl connection string de mongo es: mongodb://${txt.trim}:27017/`);
 })
 
 app.listen(port, () => {
